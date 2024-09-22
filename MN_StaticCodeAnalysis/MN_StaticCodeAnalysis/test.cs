83b6943458f6af6788941b6896b465348efe6a7c
kolotko111 <milosznowicki96@wp.pl>
2024-09-22 17:58:22 +0200

    init

---
+++ MN_StaticCodeAnalysis/MN_StaticCodeAnalysis/test.cs
@@ -0,0 +1,48 @@
+﻿namespace MNStaticCodeAnalysis
+{
+    using System.Globalization;
+
+    public class Test
+    {
+
+        private const string xxxxxx = "sd";
+        public Test()
+        {
+            Xd();
+        }
+
+        //async void do wykrywania
+        private static async void Xd()
+        {
+            if (true)
+            {
+                Xd2();
+            }
+
+            int i = 1;
+            Console.WriteLine(i);
+            Console.WriteLine(xxxxxx);
+            try
+            {
+                await Task.Delay(0);
+            }
+            catch (Exception exz)
+            {
+                Console.WriteLine(exz.ToString());
+            }
+
+            using HttpClient x = new();
+        }
+
+        private static async void Xd2()
+        {
+            await Task.Delay(0);
+
+            string uuu = "";
+            for (int i = 0; i < 10; i++)
+            {
+                uuu += "essa";
+            }
+        }
+    }
+}
