1c4e58f9754beae4f80b4e677ba2afb5e6a2f47b
kolotko111 <milosznowicki96@wp.pl>
2025-01-17 19:13:01 +0100

    Conditional compilation

---
+++ MN_StaticCodeAnalysis/MN_ConditionalCompilation/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.WriteLine(TestNamespace.CTestNamespaceName);
+
+internal static class TestNamespace
+{
+#if BRANCH_MAIN
+    public const string CTestNamespaceName = "MainNamespace";
+#elif BRANCH_TEST
+    public const string CTestNamespaceName = "TestNamespace";
+#else
+    public const string CTestNamespaceName = "DevNamespace";
+#endif
+    
+}
